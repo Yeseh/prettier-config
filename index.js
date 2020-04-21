836418cab95d2749887dd927331eeb72f0d1e3be
Yeseh <dev@jessewellenberg.nl>
2020-04-21 16:54:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    useTabs: false,
+    bracketSpacing: false,
+    arrowParens: 'always',
+    tabWidth: 4,
+};
