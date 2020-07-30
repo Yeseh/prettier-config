d57c37b979077f1431dc3b2f7cdefc8ae01450e0
Jesse Wellenberg <jesse.wellenberg@axians.com>
2020-07-30 21:24:24 +0200

    changes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 module.exports = {
     printWidth: 80,
     semi: true,
-    singleQuote: false,
     trailingComma: 'es5',
     useTabs: false,
     bracketSpacing: false,
-    arrowParens: 'always',
     tabWidth: 4,
 };
